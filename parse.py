a887859f8f7c8610550032c370ba0a5d06516ded
Anshul <anshulgo@iitk.ac.in>
2016-02-24 21:25:00 +0530

    CSV file generated

--- parse.py
+++ parse.py
@@ -8,6 +8,7 @@ path = "/home/anshul/Machine-Learning-for-Investing/intraQuarter"
 def Key_Stats(gather = "Total Debt/Equity (mrq)"):
 	stats_path = path + "/_KeyStats"
 	stock_list = [x[0] for x in os.walk(stats_path)]
+	df = pd.DataFrame(columns = ['Date','Unix','Ticker','DE Ratio'])
 	for each_dir in stock_list[1:]:
 		each_file = os.listdir(each_dir)
 		ticker = each_dir.rsplit("/")[6]
@@ -16,14 +17,17 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
 				date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
 				unix_time = time.mktime(date_stamp.timetuple())
 				full_file_path = each_dir + '/' + file
-				print full_file_path
+				# print full_file_path
 				source = open(full_file_path,'r').read()
 				try:
-					value = source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
+					value = float(source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+					df = df.append({'Date' : date_stamp,'Unix' : unix_time, 'Ticker' : ticker, 'DE Ratio' : value}, ignore_index = True)
 				except:
 					pass
-				print ticker + ": " + value
-
-		time.sleep(1)
+				# print ticker + ": " + value
+	save = gather.replace(' ','').replace(')','').replace('(','').replace('/','') + '.csv'
+	print save
+	df.to_csv(save)	
+		# time.sleep(1)
 
 Key_Stats()

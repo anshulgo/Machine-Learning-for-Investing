570b501e1f77aa7ddae76ad70f67caf521ae8d0f
Anshul <anshulgo@iitk.ac.in>
2016-02-25 17:17:30 +0530

    SP 500 and stock price added to CSV file

--- parse.py
+++ parse.py
@@ -9,6 +9,9 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
 	stats_path = path + "/_KeyStats"
 	stock_list = [x[0] for x in os.walk(stats_path)]
 	df = pd.DataFrame(columns = ['Date','Unix','Ticker','DE Ratio'])
+	
+	sp500_df = pd.DataFrame.from_csv("YAHOO-INDEX_GSPC.csv")
+
 	for each_dir in stock_list[1:]:
 		each_file = os.listdir(each_dir)
 		ticker = each_dir.rsplit("/")[6]
@@ -21,7 +24,25 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
 				source = open(full_file_path,'r').read()
 				try:
 					value = float(source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
-					df = df.append({'Date' : date_stamp,'Unix' : unix_time, 'Ticker' : ticker, 'DE Ratio' : value}, ignore_index = True)
+					 
+					try:
+					 	sp500_date = datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d')
+					 	row = sp500_df[(sp500_df.index == sp500_date)]
+					 	sp500_value = float(row["Adj Close"])
+					except:
+					 	sp500_date = datetime.fromtimestamp(unix_time - 259200).strftime('%Y-%m-%d')
+					 	row = sp500_df[(sp500_df.index == sp500_date)]
+					 	sp500_value = float(row["Adj Close"])
+
+					stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
+					# print 'Stock price: ' ,stock_price, 'ticker', ticker 
+				
+					df = df.append({'Date' : date_stamp,
+									'Unix' : unix_time,
+									'Ticker' : ticker,
+									'DE Ratio' : value,
+									'Price' : stock_price,
+									'SP 500' : sp500_value}, ignore_index = True)
 				except:
 					pass
 				# print ticker + ": " + value
